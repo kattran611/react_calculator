0d905cb78373b94be0ca8fe7b7db27df5679c8e8
Katrina Tran <kat.c.tran@gmail.com>
2018-11-17 21:40:37 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,23 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  contructor() {
+    super();
+
+    this.stat = {
+      result: ""
+    }
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <div className="calculator-body">
+          <h1>Calculator</h1>
+          <ResultComponent result={this.state.result}/>
+          <KeyPadComponent onClick={this.onClick}/>
+        </div>
       </div>
     );
   }
